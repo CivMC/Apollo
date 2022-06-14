f2b51be8a6b048d5d32c2585e5e63e68c8f996f9
Cola <vaultslave@gmail.com>
2022-06-14 02:33:39 +0100

    Gradlefication & 1.18

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+import net.civmc.civgradle.CivGradleExtension
+
+plugins {
+    `java-library`
+    id("net.civmc.civgradle") version "2.+"
+}
+
+configure<CivGradleExtension> {
+    pluginName = project.property("pluginName") as String
+}
+
+repositories {
+    mavenCentral()
+
+    maven("https://jitpack.io")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+}
+
+dependencies {
+    compileOnly("net.md-5:bungeecord-api:1.18-R0.1-SNAPSHOT")
+    compileOnly("net.civmc:zeus:2.0.0-SNAPSHOT")
+}
